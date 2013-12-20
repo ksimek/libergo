c68675c2e7185e2836eabe04463f266d5ba4ebf9
Kyle Simek <ksimek@email.arizona.edu>
2013-12-20 14:44:18 -0700

    updated MH and HMC steps to receive RNG in constructor

---
+++ ergo/def.h
@@ -0,0 +1,21 @@
+#ifndef ERGO_DEF_H
+#define ERGO_DEF_H
+
+#if __cplusplus > 199711L
+#include <memory>
+namespace ergo 
+{
+    using std::shared_ptr;
+    using std::make_shared;
+}
+#else
+#include <boost/shared_ptr.hpp>
+namespace ergo 
+{
+    using boost::shared_ptr;
+    using boost::make_shared;
+}
+#endif
+
+#endif // ERGO_DEF_H
+
